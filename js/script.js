4adb1ce73d944cbd5af14af258b70daf814f76db
sourovmodak06 <sourovmodak650@gmail.com>
2023-03-18 00:10:20 +0600

    Calculator done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('hi');
+const outputScreen = document.getElementById('output-screen');
+const message = document.getElementById('message');
+const display = (num) =>{
+    outputScreen.value += num;
+}
+const calculate = () =>{
+    if (outputScreen.value === "") {
+        message.innerText = 'Please enter number ❌';
+    } else {
+        message.innerText = '';
+        outputScreen.value = eval(outputScreen.value)
+    }
+}
+const clears = () =>{
+    outputScreen.value = "";
+}
+const del = () =>{
+    outputScreen.value = outputScreen.value.slice(0,-1);
+}
